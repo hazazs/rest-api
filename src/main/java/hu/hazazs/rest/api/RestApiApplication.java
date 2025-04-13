b18dc515ed59e1a6e9991e2b66da854181f8eb0f
hazazs <hazafi.tibor@gmail.com>
2025-04-13 22:42:30 +0200

    Add Coffee class to RestApiApplication

--- src/main/java/hu/hazazs/rest/api/RestApiApplication.java
+++ src/main/java/hu/hazazs/rest/api/RestApiApplication.java
@@ -3,11 +3,41 @@ package hu.hazazs.rest.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.UUID;
+
 @SpringBootApplication
 public class RestApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
+	}
+
+}
+
+class Coffee {
+
+	private final String id;
+	private String name;
+
+	public Coffee(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Coffee(String name) {
+		this(UUID.randomUUID().toString(), name);
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 }
