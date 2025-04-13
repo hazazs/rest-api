70f11f99104487356041dcb8644fae8fb310bf48
hazazs <hazafi.tibor@gmail.com>
2025-04-13 22:47:39 +0200

    Add /coffees GET endpoint to RestApiDemoController

--- src/main/java/hu/hazazs/rest/api/RestApiApplication.java
+++ src/main/java/hu/hazazs/rest/api/RestApiApplication.java
@@ -2,6 +2,7 @@ package hu.hazazs.rest.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -57,6 +58,11 @@ class RestApiDemoController {
 				new Coffee("Café Lareño"),
 				new Coffee("Café Três Pontas")
 		));
+	}
+
+	@GetMapping("/coffees")
+	Iterable<Coffee> getCoffees() {
+		return coffees;
 	}
 
 }
