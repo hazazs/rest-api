8d614e84e3e6a9ec2d00ef55bdb49049692de518
Tibor Hazafi (ext.) <tibor.hazafi@partner.bmwgroup.com>
2025-05-16 16:50:56 +0200

    Switch to @ConfigurationProperties from @Value
    As a result of the @ConfigurationProperties annotation all the (default) values from the application.yaml file (defined under the provided prefix) will be mapped to the respective (with the same name) member variable.

--- src/main/java/hu/hazazs/rest/api/controller/GreetingController.java
+++ src/main/java/hu/hazazs/rest/api/controller/GreetingController.java
@@ -1,6 +1,7 @@
 package hu.hazazs.rest.api.controller;
 
-import org.springframework.beans.factory.annotation.Value;
+import hu.hazazs.rest.api.dto.Greeting;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,20 +12,21 @@ import java.util.Optional;
 @RequestMapping("/greeting")
 public class GreetingController {
 
-    @Value("${greeting-name:Mirage}")
-    private String name;
+    private final Greeting greeting;
 
-    @Value("${greeting-coffee:${greeting-name} is drinking Café Ganador}")
-    private String coffee;
+    @Autowired
+    public GreetingController(Greeting greeting) {
+        this.greeting = greeting;
+    }
 
     @GetMapping
     Optional<String> getGreeting() {
-        return Optional.ofNullable(name);
+        return Optional.ofNullable(greeting.getName());
     }
 
     @GetMapping("/coffee")
     Optional<String> getNameAndCoffee() {
-        return Optional.ofNullable(coffee);
+        return Optional.ofNullable(greeting.getCoffee());
     }
 
 }
