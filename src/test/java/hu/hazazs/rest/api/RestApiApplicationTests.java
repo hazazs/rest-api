32d709363adb15df30335e50a2247f518345cd71
hazazs <hazafi.tibor@gmail.com>
2025-10-19 16:08:22 +0200

    Add unit test for the /droid endpoint

--- src/test/java/hu/hazazs/rest/api/RestApiApplicationTests.java
+++ src/test/java/hu/hazazs/rest/api/RestApiApplicationTests.java
@@ -1,13 +1,32 @@
 package hu.hazazs.rest.api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static hu.hazazs.rest.api.RestApiApplicationTests.ID;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(properties = "droid.id=" + ID)
 class RestApiApplicationTests {
 
+	static final String ID = "R2-D2";
+
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void testDroid() throws Exception {
+		mockMvc.perform(get("/droid"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.id").value("R2-D2"));
 	}
 
 }
