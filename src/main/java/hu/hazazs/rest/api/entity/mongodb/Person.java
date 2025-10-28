dcd4459c363e5efe918bf128e13346646d3e9237
hazazs <hazafi.tibor@gmail.com>
2025-10-28 19:21:15 +0100

    Refactor #1 Remove unnecessary @AllArgsConstructor

--- src/main/java/hu/hazazs/rest/api/entity/mongodb/Person.java
+++ src/main/java/hu/hazazs/rest/api/entity/mongodb/Person.java
@@ -1,13 +1,11 @@
 package hu.hazazs.rest.api.entity.mongodb;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
 @Data
-@AllArgsConstructor
 public class Person {
 
     @Id
